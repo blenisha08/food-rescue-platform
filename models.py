f2e3253f8e2da0e91905da8e4e72321991bfe556
caraldcunha <caraldcunha475@gmail.com>
2025-09-16 06:23:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -30,3 +30,15 @@ class Food(db.Model):
 
     def __repr__(self):
         return f"<Food {self.title}>"
+from datetime import datetime
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+    type = db.Column(db.String(50), nullable=False)  # 'restaurant_offer', 'ngo_acceptance', 'public_alert'
+    is_read = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='notifications')
+
